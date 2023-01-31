ed3df068cbeb395bbd45b9881fdb4fc217261b82
Isaí <jisaiherrera@gmail.com>
2023-01-31 12:20:05 +0100

    cambios en la interfaz

--- stocks.py
+++ stocks.py
@@ -2,7 +2,7 @@ from flask import Blueprint, redirect, render_template, request, url_for, sessio
 from templates.auth import login_required
 import db
 from models import Producto, Proveedor
-
+from db import get_db
 
 bp = Blueprint('stocks', __name__)
 
@@ -38,6 +38,26 @@ def crear():
                         proveedor=request.form['contenido_proveedor'])
 
     db.session.add(producto)
+    db.session.commit()
+    return redirect(url_for('stocks.inventario'))
+
+
+@bp.route('/editar-producto/<id>')
+def editar(id):
+    producto = get_db().execute('SELECT * FROM producto WHERE id = ?', (id,)).fetchone()
+    print(producto)
+    return render_template('stocks/editProduct.html', producto=producto)
+
+
+@bp.route('/editProduct/<id>', methods=['POST'])
+def edit(id):
+    producto = Producto(descripcion=request.form['new_descripcion'],
+                        stock=request.form['new_stock'],
+                        capacidad=request.form['new_capacidad'],
+                        precio=request.form['new_precio'],
+                        categoria=request.form['new_categoria'],
+                        proveedor=request.form['new_proveedor'])
+    db.session.update(producto)
     db.session.commit()
     return redirect(url_for('stocks.inventario'))
 
